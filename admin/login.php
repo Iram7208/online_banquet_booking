4fd486fd53d930bc8a46549f827e4b5073eb227f
Iram7208 <155520624+Iram7208@users.noreply.github.com>
2024-02-26 11:14:35 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -60,7 +60,7 @@ echo "<script>alert('Invalid Details');</script>";
                                     Online Banquet Booking System.
                                 </p>
                                 <p class="font-italic text-white-op">
-                                    Copyright &copy; <span class="js-year-copy">2020</span>
+                                    Copyright &copy; <span class="js-year-copy">2024</span>
                                 </p>
                             </div>
                         </div>
