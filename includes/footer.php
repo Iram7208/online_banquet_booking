2c59f350ba52140473e1adeacdef8e009e36541c
Iram7208 <155520624+Iram7208@users.noreply.github.com>
2024-03-22 15:32:56 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <!-- copyright -->
     <div class="agileits-w3layouts">
         <div class="container">
-            <p>© 2024 Online Banquet Booking System. All rights reserved | </p>
+            <p>© 2024 Online Banquet Booking System by IRAM ANSARI. All rights reserved | </p>
         </div>
     </div>
     <!-- //copyright -->
